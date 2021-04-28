5dae42deb5ecafcc0d702f4491ea63eda9a05f96
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-04-28 14:06:16 +0300

    Update nbit.py

--- nbit.py
+++ nbit.py
@@ -2,24 +2,19 @@ from itertools import product
 arr = 0
 val_x = []
 val_y =[]
-
 n = int(input('n = '))
 
 if __name__ == "__main__":
 
-    for x in range (0, n + 1):
-        for y in range (0, n + 1):
+    for x in range(n + 1):
+        for y in range(n + 1):
             if y == 0:
                 arr += 1
         if arr == 1:
-            val_x.append(x)
-            val_y.append(x)
+
+            val_x.append(f"{x:02b}")
+            val_y.append(f"{x:02b}")
         arr = 0
 
-
-    res = list(product(val_x, val_y))
-    
-
-    print(res)
-
-    print("--- --- --- ---")
+res = list(product(val_x, val_y))
+print(res)
