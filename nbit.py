5990c750f83a168fc10c48711e79b3c66cd7fe7a
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-05-12 17:42:17 +0300

    Update nbit.py

--- nbit.py
+++ nbit.py
@@ -68,3 +68,42 @@ if __name__ == "__main__":
 endmodule""")
 
     fileobject.close()
+
+    filename = "multi_TB.v"
+
+    fileobject = open(filename,'w')
+        
+    fileobject.write(f"""`timescale 1ns / 1ps
+
+module mempy_TB;
+
+ parameter N = {n};
+
+  reg [N - 1:0] address;
+  reg read_en, ce;
+  wire [N - 1:0] data;
+ 
+ integer i;
+  
+mempy dut(.address(address),
+          .data(data),
+          .read_en(read_en),
+          .ce(ce));
+
+ initial begin
+   address = 0;
+   read_en = 0;
+   ce      = 0;
+   #10 $monitor ("address = %h, data = %h, read_en = %b, ce = %b", address, data, read_en, ce);
+   for (i = 0; i < 2**(2*N); i = i + 1 )begin
+     #5 address = i;
+     read_en = 1;
+     ce = 1;
+     #5 read_en = 0;
+     ce = 0;
+     address = 0;
+   end
+ end
+endmodule""")
+
+    fileobject.close()
