10714623fc179e4512b8c1c549b5894f350cace0
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-05-13 11:00:46 +0300

    Update mempy_TB.v

--- mempy_TB.v
+++ mempy_TB.v
@@ -2,13 +2,13 @@
 
 module mempy_TB;
 
- parameter N = 8;
+ parameter N = 2;
 
-  reg [N - 1:0] address;
+  reg [(2*N)-1:0] address;
   reg read_en, ce;
-  wire [N - 1:0] data;
+  wire [(2*N)-1:0] data;
  
- integer i;
+ integer i,j;
   
 mempy dut(.address(address),
           .data(data),
@@ -21,12 +21,14 @@ mempy dut(.address(address),
    ce      = 0;
    #10 $monitor ("address = %h, data = %h, read_en = %b, ce = %b", address, data, read_en, ce);
    for (i = 0; i < 2**(2*N); i = i + 1 )begin
-     #5 address = i;
+     for (j = 0; j < 2**(2*N); j = j + 1)begin
+     #5 address = {i,j};
      read_en = 1;
      ce = 1;
      #5 read_en = 0;
      ce = 0;
      address = 0;
-   end
+        end
+    end
  end
 endmodule
