c83346e0f1a4d73a69f828320c44f1fe782c8207
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-05-13 11:00:15 +0300

    Update mempy.v

--- mempy.v
+++ mempy.v
@@ -1,17 +1,14 @@
-module mempy #(parameter N = 8)
-    (input [N - 1:0] address,
+module mempy #(parameter N = 2)
+    (input [(2*N) - 1:0] address,
      input read_en,
      input ce,
-     output [N - 1:0] data);
+     output [(2*N) - 1:0] data);
               
- reg [N - 1:0] mem [0: 2**(2*N)-1];
- integer file_pointer;
+ reg [(2*N)-1:0] mem [0:2**(2*N)-1];
 
   assign data = (ce && read_en) ? mem[address] : 0;
-
- integer i;
   
     initial begin
-  $readmemb("rom_256x8.dat", mem);
+  $readmemb("rom_16x4.dat", mem);
     end
 endmodule
