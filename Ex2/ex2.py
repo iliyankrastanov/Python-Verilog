c9831de702b8d248ea548808e6716bb4a54c319c
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-05-20 15:04:34 +0300

    Create ex2.py

---
+++ Ex2/ex2.py
@@ -0,0 +1,35 @@
+filename = "{0}_{1}.txt"
+name = filename.format(f'{"Pass"}', f'{"Fail"}')
+
+f = open(name,'w')
+f1 = open("rom_16x4.dat", "r")
+f2 = open("results.dat", "r")  
+i = 0
+  
+for line1 in f1:
+    i += 1
+      
+    for line2 in f2:
+          
+        # matching line1 from both files
+        if line1 == line2:  
+            print(" Line ", i, ": PASS") 
+            f.write("PASS\n") 
+            
+                
+        else:
+            print(" Line ", i, ":")
+            # else print that line from both files
+            print("\tFAIL")
+            print("\tFile 1:", line1, end='')
+            print("\tFile 2:", line2, end='')
+            f.write(f"Different found in Line {i}\n " )
+            f.write(f"Line {i} in File1)--> {line1} ")
+            f.write(f"Line {i} in File2)--> {line2}")
+            
+        break
+      
+# closing files
+f1.close()                                       
+f2.close() 
+f.close()
