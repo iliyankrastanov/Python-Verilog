b27f844d355824fea5111b7de6f6db6271931ea1
Iliyan <iliyan97@abv.bg>
2021-05-20 14:41:38 +0300

    Ex2

--- Ex2/ex2.py
+++ Ex2/ex2.py
@@ -1,7 +1,10 @@
 # reading files
-f1 = open("rom_16x4.dat", "r")  
+filename = "{0}_{1}.txt"
+name = filename.format(f'{"Pass"}', f'{"Fail"}')
+
+f = open(name,'w')
+f1 = open("rom_16x4.dat", "r")
 f2 = open("results.dat", "r")  
-f = open("results_compared.txt","w+")  
 i = 0
   
 for line1 in f1:
@@ -11,22 +14,22 @@ for line1 in f1:
           
         # matching line1 from both files
         if line1 == line2:  
-            print("Line ", i, ": IDENTICAL") 
-            f.write("Identical \n ") 
+            print(" Line ", i, ": PASS") 
+            f.write("PASS\n") 
             
                 
         else:
-            print("Line ", i, ":")
+            print(" Line ", i, ":")
             # else print that line from both files
+            print("\tFAIL")
             print("\tFile 1:", line1, end='')
             print("\tFile 2:", line2, end='')
-            f.write(f"Different found in Line {i}  \n " )
+            f.write(f"Different found in Line {i}\n " )
             f.write(f"Line {i} in File1)--> {line1} ")
-            f.write(f"Line {i} in File2)--> {line2} \n ")
+            f.write(f"Line {i} in File2)--> {line2}")
             
         break
-        
-
+      
 # closing files
 f1.close()                                       
 f2.close() 
